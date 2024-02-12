e9257d991a7de9c7c075f9dd1ee44ade980088ff
Cristian <skylerhunter6@gmail.com>
2024-02-12 16:28:24 +0100

    Added route handling

--- adapter_layer/auth_db_interface/server.js
+++ adapter_layer/auth_db_interface/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const PORT = process.env.AUTH_DB_ADAPTER || 3000;
 
+const registrationRouter = require("./routes/registrationRoute");
+const loginRouter = require("./routes/loginRoute");
+
 const server = express();
 
+server.use(express.json());
+server.use('/register', registrationRouter);
+server.use('/login', loginRouter);
 
 
 server.listen(PORT, () => {
