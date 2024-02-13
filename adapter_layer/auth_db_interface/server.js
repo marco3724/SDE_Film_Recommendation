52dfc59738262db8708ff2c04bec671688611937
Cristian <skylerhunter6@gmail.com>
2024-02-13 16:14:00 +0100

    imported sequelize library

--- adapter_layer/auth_db_interface/server.js
+++ adapter_layer/auth_db_interface/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const PORT = process.env.AUTH_DB_ADAPTER || 3000;
+const sequelize = require("./database/dbSettings");
+const PORT = process.env.AUTH_ADAPTER_PORT || 3000;
 
 const registrationRouter = require("./routes/registrationRoute");
 const loginRouter = require("./routes/loginRoute");
@@ -10,6 +11,11 @@ server.use(express.json());
 server.use('/register', registrationRouter);
 server.use('/login', loginRouter);
 
+try {
+    sequelize.sync({ force: true });
+} catch (error) {
+    console.error("Failing sync with db: ", error);
+}
 
 server.listen(PORT, () => {
     console.log(`Authentication DB Adapter Server is running on port ${PORT}`);
