e5a0cc58bd8bca941c69e7bb2a1b373dc1d73f6a
Cristian <skylerhunter6@gmail.com>
2024-02-15 17:30:07 +0100

    Moved mail and password validation in business layer

--- business_logic_layer/registration/controllers/registrationController.js
+++ business_logic_layer/registration/controllers/registrationController.js
@@ -1,4 +1,6 @@
 const fetch = require("node-fetch");
+const emailValidator = require("email-validator");
+const PW_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
 const AUTH_DB_ADAPTER_ENDPOINT = `http://auth_db_adapter:${process.env.AUTH_ADAPTER_PORT}/register`;
 
@@ -11,35 +13,45 @@ exports.register = async (request, response) => {
         });
     }
 
-    let userData = {
-        userName: userName,
-        email: email,
-        password: password
-    };
+    // email and password validation
+    const isEmailValid = emailValidator.validate(email);
+    const isPasswordValid = PW_REGEX.test(password);
 
-    let result;
-    try {
-        const res = await fetch(AUTH_DB_ADAPTER_ENDPOINT, {
-            method: 'POST',
-            body: JSON.stringify(userData),
-            headers: {'Content-Type' : 'application/json'}
-        });
-
-        result = await res.json();
-
-    } catch (error) {
-        return response.status(400).send({
-            message: "Oops something went wrong :("
-        });
-    }
-
-    if (result) {
-        return response.status(200).send({
-            message: "Registration confirmed"
+    if (!isEmailValid || !isPasswordValid) {
+        return response.status(500).send({
+            message: "Email or password not valid!"
         });
     } else {
-        return response.status(400).send({
-            message: "Oops something went wrong :(, during the registration"
-        });
+        let userData = {
+            userName: userName,
+            email: email,
+            password: password
+        };
+
+        let result;
+        try {
+            const res = await fetch(AUTH_DB_ADAPTER_ENDPOINT, {
+                method: 'POST',
+                body: JSON.stringify(userData),
+                headers: {'Content-Type' : 'application/json'}
+            });
+    
+            result = await res.json();
+    
+        } catch (error) {
+            return response.status(400).send({
+                message: "Oops something went wrong :("
+            });
+        }
+
+        if (result) {
+            return response.status(200).send({
+                message: "Registration confirmed"
+            });
+        } else {
+            return response.status(400).send({
+                message: "Oops something went wrong :(, during the registration"
+            });
+        }
     }
 };
