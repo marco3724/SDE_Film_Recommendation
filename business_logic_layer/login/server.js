317c05b249fee0c628bca0889d287c43a2400513
Cristian <skylerhunter6@gmail.com>
2024-02-15 10:27:59 +0100

    Added parser for body requests

--- business_logic_layer/login/server.js
+++ business_logic_layer/login/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const loginRouter = require("./routes/loginRoute");
 const PORT = process.env.LOGIN_BUSINESS_PORT || 3001;
 
 const server = express();
 
+server.use(bodyParser.json());
 server.use("/login", loginRouter);
 
 server.listen(PORT, () => {
