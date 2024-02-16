8339e34873b671ee9ce93cf79b79de81010d7260
marco <marcowang05@gmail.com>
2024-02-16 11:37:21 +0100

    default port change

--- adapter_layer/tmdb_detail_api/server.js
+++ adapter_layer/tmdb_detail_api/server.js
@@ -4,7 +4,7 @@ const app = express();
 const router = require('./routes/detail_routes')
 
 app.use("/tmdb_film", router);
-const PORT = process.env.TMDB_ADAPTER_PORT || 3000;
+const PORT = process.env.TMDB_ADAPTER_PORT || 3001;
 console.log(process.env.TMDB_ADAPTER_PORT)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
