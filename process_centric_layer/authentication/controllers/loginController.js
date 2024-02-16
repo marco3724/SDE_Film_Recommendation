ab09de9d0bd06874d72a5ae0194d43084bacab11
Cristian <skylerhunter6@gmail.com>
2024-02-16 12:16:50 +0100

    Implemented verification logic

--- process_centric_layer/authentication/controllers/loginController.js
+++ process_centric_layer/authentication/controllers/loginController.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const LOGIN_BUSINESS_ENDPOINT = `http://login_business:${process.env.LOGIN_BUSINESS_PORT}/login`;
+const VERIFICATION_TOKEN_ENDPOINT = LOGIN_BUSINESS_ENDPOINT + "/verify-token";
 
 exports.loginProcedure = async (request, response) => {
     const { email, password } = request.body;
@@ -32,11 +33,12 @@ exports.loginProcedure = async (request, response) => {
         });
     }
 
-    if (result) {
-        console.log(result);
-        return response.status(200).send({
-            message: "Login successful"
-        });
+    if (result.status == "success") {
+        return response.cookie("token", result.jwtToken, {
+            httpOnly: true,
+            sameSite: true,
+            secure: false // da settare a true? funziona anche con http?
+        }).status(200).json({ message: "Successfull login"});
     } else {
         console.log(result);
         return response.status(400).send({
@@ -44,3 +46,49 @@ exports.loginProcedure = async (request, response) => {
         });
     }
 };
+
+exports.verifyAuthentication = async (request, response) => {
+    const token = request.cookies["token"];
+
+    if (!token) {
+        return response.status(200).json({
+            status: "success",
+            authenticated: false
+        });
+    }
+
+    let bodyToken = {
+        tkn: token
+    }
+
+    let validation;
+
+    try {
+        const req = await fetch(VERIFICATION_TOKEN_ENDPOINT, {
+            method: "POST",
+            body: JSON.stringify(bodyToken),
+            headers: { 'Content-Type' : 'application/json'}
+        });
+
+        validation = await req.json();
+    } catch (error) {
+        return response.status(400).json({
+            status: "unsuccessful",
+            message: "Oops something went wrong"
+        });
+    }
+
+    if (validation.status == "success") {
+        return response.status(200).json({
+            status: "success",
+            isAuthenticated: validation.isAuthenticated,
+            plain_token: validation.plain_token
+        });
+    } else {
+        return response.status(200).json({
+            status: "success",
+            isAuthenticated: validation.isAuthenticated
+        });
+    }
+
+}
