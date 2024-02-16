23e08661c75241955d57eb039643a9628227122f
marco <marcowang05@gmail.com>
2024-02-16 12:36:47 +0100

    detail business layer

---
+++ business_logic_layer/details/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const router = require('./routes/details_routes')
+
+app.use("/full_details_film", router);
+const PORT = process.env.TMDB_ADAPTER_PORT || 3002;
+console.log(process.env.TMDB_ADAPTER_PORT)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
