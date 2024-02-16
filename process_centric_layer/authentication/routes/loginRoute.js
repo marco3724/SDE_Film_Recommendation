10497164f531f3601bc5dafaa55e7dc155f08679
Cristian <skylerhunter6@gmail.com>
2024-02-16 12:16:33 +0100

    Route to check whether a user is currently authenticated

--- process_centric_layer/authentication/routes/loginRoute.js
+++ process_centric_layer/authentication/routes/loginRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { loginProcedure } = require("../controllers/loginController.js");
+const { loginProcedure, verifyAuthentication } = require("../controllers/loginController.js");
 const loginRouter = express.Router();
 
 loginRouter.post("/", loginProcedure);
+loginRouter.post("/isAuthenticated", verifyAuthentication);
+
 
 module.exports = loginRouter
