beff86141c3f02f2e7d8fd5bcc341226cf0172d1
Cristian <skylerhunter6@gmail.com>
2024-02-16 12:14:55 +0100

    cookie parser

--- process_centric_layer/authentication/server.js
+++ process_centric_layer/authentication/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
 const PORT = process.env.AUTHENTICATION_PROCESS_PORT || 3003;
 const registrationRouter = require("./routes/registrationRoute");
 const loginRouter = require("./routes/loginRoute");
@@ -7,6 +9,7 @@ const loginRouter = require("./routes/loginRoute");
 const server = express();
 
 server.use(bodyParser.json());
+server.use(cookieParser());
 server.use('/register', registrationRouter);
 server.use('/login', loginRouter);
 
