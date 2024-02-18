8bbc119689b3c38f8044266051b2116845d6bd52
Cristian <skylerhunter6@gmail.com>
2024-02-18 12:31:17 +0100

    conditional rendering for register and login form

--- frontend/src/pages/authentication/AuthenticationPage.jsx
+++ frontend/src/pages/authentication/AuthenticationPage.jsx
@@ -1,11 +1,35 @@
 import styles from './authPage.module.css';
 import LoginForm from "../../components/LoginForm/LoginForm";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import { useState } from 'react';
 
 const AuthenticationPage = () => {
+    const [isRegisterForm, setRegisterForm] = useState(false);
+
+
     return (
-        <div className={styles.container}>
-            <LoginForm />
-        </div>
+        <>
+            <div className={styles.container}>
+                {!isRegisterForm ?
+                    <LoginForm />
+                    :
+                    <RegisterForm />
+                }
+            </div>
+            <div className={styles.register}>
+                {!isRegisterForm ?
+                    <p>Not a member? 
+                        <a className={styles.registerLink}
+                            onClick={() => setRegisterForm(true)}> Register!</a>
+                    </p>
+                    :
+                    <p>Maybe you forgot you have an account,
+                        <a className={styles.registerLink}
+                            onClick={() => setRegisterForm(false)}> Login!</a>
+                    </p>
+                }
+            </div>
+        </>
     );
 }
 
