d281d94e1fe76a1aaade71856322f19911962e4f
Cristian <skylerhunter6@gmail.com>
2024-02-18 12:28:54 +0100

    Added cors as a dependency

--- process_centric_layer/authentication/server.js
+++ process_centric_layer/authentication/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const cors = require("cors");
 
 const PORT = process.env.AUTHENTICATION_PROCESS_PORT || 3003;
 const registrationRouter = require("./routes/registrationRoute");
@@ -8,6 +9,11 @@ const loginRouter = require("./routes/loginRoute");
 
 const server = express();
 
+// This allows us to test react from outside the docker network
+server.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 server.use(bodyParser.json());
 server.use(cookieParser());
 server.use('/register', registrationRouter);
