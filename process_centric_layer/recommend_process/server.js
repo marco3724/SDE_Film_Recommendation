c12c667e8767dfe4fb8facbda446e9cfe070c31d
Cristian <skylerhunter6@gmail.com>
2024-02-19 17:58:48 +0100

    added cookie-parser as a dependency

--- process_centric_layer/recommend_process/server.js
+++ process_centric_layer/recommend_process/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
 const app = express();
 const router = require('./routes/recommend_routes')
 
 app.use(cors({
-  origin: 'http://localhost:3000' 
+  origin: 'http://localhost:3000',
+  credentials: true
 }));
 
+app.use(cookieParser());
 app.use("/recommend_film", router);
 
 
-const PORT = process.env.RECOMMEND_PROCESS_PORT ||8006;
-//console.log(process.env.RECOMMEND_PROCESS_PORT)
+const PORT = process.env.RECOMMEND_PROCESS_PORT || 8006;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
