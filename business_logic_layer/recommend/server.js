0c293211c07b109f123b07bcb7f46838df22bafe
Cristian <skylerhunter6@gmail.com>
2024-02-20 12:44:06 +0100

    Added body parser

--- business_logic_layer/recommend/server.js
+++ business_logic_layer/recommend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
 const router = require('./routes/recommend_routes')
 
+app.use(bodyParser.json());
 app.use("/recommend_film", router);
 const PORT = process.env.RECOMMEND_PORT || 4003;
 
